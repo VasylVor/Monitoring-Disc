9d945fea3448c167705b75961a0d76635d57a21e
Vasyl Voronin <vasilvoronin0@gmail.com>
2018-08-28 17:51:31 +0300

    clone

---
+++ Installer1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : Installer
+    {
+        ServiceProcessInstaller processInstaler;
+        ServiceInstaller serviceInstaller;
+
+        public Installer1()
+        {
+            processInstaler = new ServiceProcessInstaller();
+            processInstaler.Account = ServiceAccount.LocalSystem;
+
+            serviceInstaller = new ServiceInstaller();
+            serviceInstaller.ServiceName = "[====TEST SERVICE====]";
+            serviceInstaller.Description = "My First NT (Windiws) service!!!";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            base.Installers.Add(processInstaler);
+            base.Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        DriveInfo[] drives;
+        static readonly string filePath = @"D:\DeletedFiles.txt";
+        public Service1()
+        {
+            InitializeComponent();
+
+            drives = DriveInfo.GetDrives().Where<DriveInfo>(drive => drive.DriveType == DriveType.Fixed).ToArray<DriveInfo>();          
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            foreach (var drive in drives)
+            {
+                FileSystemWatcher watcher = new FileSystemWatcher(drive.RootDirectory.ToString());
+                watcher.IncludeSubdirectories = true;
+                watcher.Deleted += watcher_Deleted;
+                watcher.Error += watcher_Error;
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private void watcher_Error(object sender, ErrorEventArgs e)
+        {
+            if(this.CanPauseAndContinue == true)
+            {
+                this.OnStart(new string[0]);
+            }
+        }
+
+        async void watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            using (var stream = File.Open(filePath, FileMode.Append, FileAccess.Write, FileShare.Read))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    await streamWriter.WriteLineAsync(e.FullPath);
+                }
+            }
+        }
+    }
+}
